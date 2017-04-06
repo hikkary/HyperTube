1be91136e16f7a73179cdda94f8990389769cc50
Andrea SALAMA <asalama@e2r6p15.42.fr>
2017-04-07 00:30:27 +0200

    added footer + fix norme front + error messages in forgot password and register

---
+++ front/src/Components/Footer/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './sass/footer.sass';
+
+export default class Footer extends React.Component {
+	render(){
+		return(
+      <div>
+        <p className="footer">asalama & zkerkeb @copyright 2017</p>
+      </div>
+		)
+	}
+}
